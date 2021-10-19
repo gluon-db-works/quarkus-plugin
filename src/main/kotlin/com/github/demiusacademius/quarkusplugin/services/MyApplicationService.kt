08f606952e31c417fdbfea6917ad8b724c35886e
GitHub Action <action@github.com>
2021-10-19 05:35:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/demiusacademius/quarkusplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.demiusacademius.quarkusplugin.services
+
+import com.github.demiusacademius.quarkusplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
